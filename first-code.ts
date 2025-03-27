1ae227bb3d3e5f815491eaeefa4393699b4fa80e
WESLEY ANJOS <wesleyanjosfullstack@gmail.com>
2025-03-27 17:02:09 -0300

    testando o classe e objeto

--- first-code.ts
+++ first-code.ts
@@ -6,3 +6,17 @@ const name = 'Wesley Anjos'
 
 console.log(`Olá ${name}, seja bem-vindo !`)
 console.log('==========')
+
+class Product 
+{
+    public name: string
+    public value: number
+
+    constructor(productName, productValue) 
+    {
+        this.name = productName
+        this.value = productValue
+    }
+}
+
+const playstation = new Product('Playstation 5', 5000)
