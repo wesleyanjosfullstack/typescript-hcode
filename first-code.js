417e381c8436c283563c236be4e84340ee3a088c
WESLEY ANJOS <wesleyanjosfullstack@gmail.com>
2025-03-27 16:54:26 -0300

    testando o typescript

--- first-code.js
+++ first-code.js
@@ -1,1 +1,5 @@
 console.log('Hello world ! TypeScript !');
+console.log('==========');
+var name = 'Wesley Anjos';
+console.log("Ol\u00E1 ".concat(name, ", seja bem-vindo !"));
+console.log('==========');
--- first-code.ts
+++ first-code.ts
@@ -1,1 +1,8 @@
 console.log('Hello world ! TypeScript !')
+
+console.log('==========')
+
+const name = 'Wesley Anjos'
+
+console.log(`Olá ${name}, seja bem-vindo !`)
+console.log('==========')
